d8b7147722cc6613260257a20590c7bbea0b7802
Prashant Tomar <YOUR_GITHUB_EMAIL>
2026-09-16 06:15:10 +0530

    Initial AniScript Pro deployment

---
+++ desktop.py
@@ -0,0 +1,21 @@
+import threading
+import time
+import webbrowser
+
+from main import app
+
+
+def open_browser():
+    time.sleep(2)
+    webbrowser.open("http://127.0.0.1:5000/")
+
+
+if __name__ == "__main__":
+    threading.Thread(target=open_browser, daemon=True).start()
+
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=False,
+        use_reloader=False
+    )
